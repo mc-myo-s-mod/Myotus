09cedf0fc05d7f5735e05d64f226abf64910adab
myogoo <myogoo@MyoGooPC.localdomain>
2026-03-23 23:35:17 +0900

    chore: sync current working tree

--- src/main/java/me/myogoo/myotus/init/MyoItems.java
+++ src/main/java/me/myogoo/myotus/init/MyoItems.java
@@ -3,6 +3,7 @@ package me.myogoo.myotus.init;
 import me.myogoo.myotus.Myotus;
 import me.myogoo.myotus.item.DiamondUpgradeCardItem;
 import net.minecraft.world.item.Item;
+import net.neoforged.fml.loading.FMLLoader;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
@@ -10,6 +11,8 @@ public class MyoItems {
 
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(Myotus.MODID);
 
-    public static final DeferredItem<DiamondUpgradeCardItem> DIAMOND_UPGRADE_CARD =
-            ITEMS.register("diamond_upgrade_card", () -> new DiamondUpgradeCardItem(new Item.Properties().stacksTo(1)));
+    public static final DeferredItem<DiamondUpgradeCardItem> DIAMOND_UPGRADE_CARD = FMLLoader.isProduction()
+            ? null
+            : ITEMS.register("diamond_upgrade_card",
+                    () -> new DiamondUpgradeCardItem(new Item.Properties().stacksTo(1)));
 }
